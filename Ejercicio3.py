6ce830c5142f368edb29c94280f891b03bd0226f
davidcasbae <david.casbae.1@educa.jcyl.es>
2024-11-18 18:18:29 +0100

    nueva version

---
+++ Ejercicio3.py
@@ -0,0 +1,29 @@
+import threading
+import time
+
+
+class SesionUsuario:
+    def iniciar_sesion(self, nombre_usuario):
+        self.nombre_usuario = nombre_usuario
+
+    def mostrar_sesion():
+        print(f'{self.nombre_usuario}')
+
+def main():
+        sesion_usuario = SesionUsuario()
+        datos_sesion = threading.local()
+
+        def gestionar_sesion(nombre_usuario):
+            datos_sesion = sesion_usuario.iniciar_sesion(nombre_usuario)
+            datos_sesion.mostrar_sesion()
+
+        hilos = []
+        nombres_usuarios = ["Alice", "Bob", "Charlie", "Diana"]
+
+        for nombre in nombres_usuarios:
+            hilo = threading.Thread(target=gestionar_sesion(nombre))
+            hilos.append(hilo)
+            hilo.start()
+
+        for hilo in hilos:
+            hilo.join()
